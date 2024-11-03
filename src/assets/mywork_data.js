4a39364c098c98650d2cdfa16ad004d04393b4fc
Aditya1or0 <adityapandit264@gmail.com>
2024-11-03 15:04:03 +0530

    updated work

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -22,7 +22,7 @@ const mywork_data = [
     w_no: 3,
     w_name: "Chatapp",
     w_img: Chatapp,
-    link: "https://chatapp.example.com/",
+    link: "chat-app-nine-ruby.vercel.app/",
   },
   {
     w_no: 4,
